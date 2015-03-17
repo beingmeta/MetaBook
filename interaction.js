138a2a356c5347a20930e6e5b7b77916173683c9
Ken Haase <kh@beingmeta.com>
2015-03-17 15:46:28 -0400

    Tweak some taptap thresholds

--- interaction.js
+++ interaction.js
@@ -185,7 +185,7 @@
             metaBook.TapHold.body=fdjtUI.TapHold(
                 fdjt.ID("METABOOKBODY"),
                 {override: true,noslip: true,id: "METABOOKBODY",
-                 maxtouches: 2,taptapmsecs: 250,
+                 maxtouches: 2,taptapmsecs: 100,
                  untouchable: externClickable,
                  movethresh: 10});
             addHandlers(metaBook.HUD,'hud');}
