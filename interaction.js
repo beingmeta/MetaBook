89eb7f19f94e02c80fbef238a7463be0197e2655
Ken Haase <kh@beingmeta.com>
2015-04-21 13:56:20 -0500

    Provide explicit false untouchable argument

--- interaction.js
+++ interaction.js
@@ -178,7 +178,7 @@
                 $ID("METABOOKBODY"),
                 {override: true,noslip: true,id: "METABOOKBODY",
                  maxtouches: 3,taptapmsecs: true,
-                 movethresh: 10});
+                 movethresh: 10,untouchable: false});
             addHandlers(metaBook.HUD,'hud');}
         if (mode) {
             var handlers=metaBook.UI.handlers[mode];
